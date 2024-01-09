f3174f6cc6bdbbb70c15665ddf2b6b7f48ecd391
mehranredrose <mehranredrose@gmail.com>
2024-01-09 23:19:17 +0330

    main.py and config.py added + gitignore file

---
+++ exchange/exchange/config.py
@@ -0,0 +1,4 @@
+BASE_PATH = 'http://data.fixer.io/api/latest?access_key='
+API_KEY = 'b1175790b5b5cd52866fc789504e1946'
+
+url = BASE_PATH+API_KEY
---
+++ exchange/exchange/main.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from config import url
+
+
+def get_rates():
+    response = requests.get(url)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
+
+
+def archive(filename, rates):
+    with open(f'archive/{filename}.json', 'w') as f:
+        f.write(json.dumps(rates))
+
+
+if __name__ == "__main__":
+    response = get_rates()
+    archive(response['timestamp'], response['rates'])
