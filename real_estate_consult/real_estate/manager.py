14026fc3d19cd89780bc87c83554459d02094bad
mehranredrose <mehranredrose@gmail.com>
2024-01-20 20:47:51 +0330

    creating an option in manager to let compare in range

--- real_estate_consult/real_estate/manager.py
+++ real_estate_consult/real_estate/manager.py
@@ -7,7 +7,24 @@ class Manager:
 
         results = list()
         for key, value in kwargs.items():
+            if key.endswith('__min'):
+                key = key[:-5]
+                compare_key = 'min'
+            elif key.endswith('__max'):
+                key = key[:-5]
+                compare_key = 'max'
+            else:
+                compare_key = 'equal'
+
             for obj in self._class.object_list:
-                if hasattr(obj, key) and getattr(obj, key) == value:
-                    results.append(obj)
+                if hasattr(obj, key):
+                    if compare_key == 'min':
+                        result = getattr(obj, key) >= value
+                    elif compare_key == 'max':
+                        result = getattr(obj, key) <= value
+                    else:
+                        result = getattr(obj, key) == value
+
+                    if result:
+                        results.append(obj)
         return results
